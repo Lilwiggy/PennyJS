8bb231931ea1ecf9a9ab0433cad75c818e54eee8
broodroosterdev <31385368+broodroosterdev@users.noreply.github.com>
2017-12-23 16:45:31 +0100

    Changed the setbackground command
    To avoid spaghetti code and allow easy customizability without changing code i made it so the command would read from the shop.json file. This way adding backgrounds are going to be very easy (for the setbackground command at least)
    
    Note: This change hasnt been fully tested yet.

--- commands/setbackground.js
+++ commands/setbackground.js
@@ -1,86 +1,56 @@
+var fs = require('fs');
+var json = JSON.parse(fs.readFileSync('../modules/shop.json'));
+var backgrounds = [];
 exports.run = function (client, message,args, Discord, connection){
-//Background thingies! (needs more)
-
+//Background thingies!
+for (var key in json) {
+    if (json.hasOwnProperty(key)) {
+        var item = json[key];
+        if(!item.type == 'background') return;
+        else if(item.name == 'patreon') return;
+        else if(item.type == 'background'){
+        backgrounds.push({
+            "image": item.image,
+            "price": item.price,
+            "name": item.name,
+        });
+}
+}
+}
 
 if(args.length === 1){
-    message.channel.send("Usage: //setbackground [background]\nBackground options are here: https://pennybot.tk/backgrounds")
-} else {
-      if(args[1].toLowerCase() === 'adam'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "adam", 500, message) 
-    } else if(args[1].toLowerCase() === 'glynda'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "glynda", 1000, message) 
-    } else if(args[1].toLowerCase() === 'mercury'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "mercury", 1000, message) 
-    } else if(args[1].toLowerCase() === 'midna'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "midna", 1000, message) 
-    } else if(args[1].toLowerCase() === 'ironwood'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "ironwood", 1000, message) 
-    } else if(args[1].toLowerCase() === 'ren'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "ren", 2000, message) 
-    } else if(args[1].toLowerCase() === 'raven'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "raven", 2000, message) 
-    } else if(args[1].toLowerCase() === 'ruby'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "ruby", 2000, message) 
-    } else if(args[1].toLowerCase() === 'weiss'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "weiss", 2000, message) 
-    } else if(args[1].toLowerCase() === 'yang'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "yang", 2000, message) 
-    } else if(args[1].toLowerCase() === 'blake'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "blake", 2000, message) 
-    } else if(args[1].toLowerCase() === 'jaune'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "jaune", 2000, message) 
-    } else if(args[1].toLowerCase() === 'cinder'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "cinder", 2000, message) 
-    } else if(args[1].toLowerCase() === 'emerald'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "emerald", 2000, message) 
-    }  else if(args[1].toLowerCase() === 'velvet'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "velvet", 2000, message) 
-    } else if(args[1].toLowerCase() === 'pyrrha'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "pyrrha", 2000, message) 
-    } else if(args[1].toLowerCase() === 'roman'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "roman", 2000, message) 
-    } else if(args[1].toLowerCase() === 'yatsu'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "yatsu", 3000, message) 
-    } else if(args[1].toLowerCase() === 'neo'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "neo", 3000, message) 
-    } else if(args[1].toLowerCase() === 'penny'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "penny", 3000, message) 
-    } else if(args[1].toLowerCase() === 'zwei'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "zwei", 3000, message) 
-    } else if(args[1].toLowerCase() === 'qrow'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "qrow", 4000, message) 
-    } else if(args[1].toLowerCase() === 'nora'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "nora", 4000, message) 
-    }  else if(args[1].toLowerCase() === 'ozpin'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "ozpin", 4000, message) 
-    } else if(args[1].toLowerCase() === 'kyoko'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "kyoko", 2000, message) 
-    } else if(args[1].toLowerCase() === 'hina'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "hina", 2000, message) 
-    } else if(args[1].toLowerCase() === 'monokuma'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "monokuma", 5000, message) 
-    } else if(args[1].toLowerCase() === 'patreon'){
-        connection.query("SELECT * FROM `User` WHERE `User_ID` = "+message.author.id+"", function(err, res, fields){
-            if(res[0].patron === 1){
-                client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "patreon", 0, message)
-            } else {
-                message.channel.send("This background is for Patreons only.")
+    return message.channel.send("Usage: //setbackground [background]\nBackground options are here: https://pennybot.tk/backgrounds")
+}
+
+switch(args[1].toLowerCase()) {
+    case 'patreon':{
+            connection.query("SELECT * FROM `User` WHERE `User_ID` = "+message.author.id+"", function(err, res, fields){
+                if(res[0].patron === 1){
+                    client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "patreon", 0, message)
+                } else {
+                    message.channel.send("This background is for Patreons only.")
+                }
+            })
+            break;}
+    default:{
+                try{
+                    var background = backgrounds.filter(item => item.name == "patreon")
+                    background
+                    client.setBackground(message.author.id, message.author.username, message.author.avatarURL, backgrounds[0].name, backgrounds[0].price, message)
+
+                }catch (e){
+                    message.channel.send("Background options can be found here: http://pennybot.tk/backgrounds")
+                }
+            break;         
             }
-        })
-         
-    } else if(args[1].toLowerCase() === 'ibuki'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "ibuki", 4000, message) 
-    } else if(args[1].toLowerCase() === 'chiaki'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "chiaki", 5000, message) 
-    } else if(args[1].toLowerCase() === 'junko'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "junko", 4000, message) 
-    } else if(args[1].toLowerCase() === 'earthchan'){
-        client.setBackground(message.author.id, message.author.username, message.author.avatarURL, "earthchan", 5, message) 
-    } else {
-        message.channel.send("Background options can be found here: http://pennybot.tk/backgrounds")
     }
+      
+        
+         
+   
+        
 }
-}
+
 exports.conf = {
     name: "setbackground",
     category: "Miscelaneous",
