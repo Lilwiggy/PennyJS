b38ab0995d7744b164334435a023c5c7f4cb5c55
John Wiggins <lilwiggy123@gmail.com>
2017-12-26 20:38:36 -0600

    Custom events for servers

---
+++ events/guildBanAdd.js
@@ -0,0 +1,13 @@
+exports.run = (client, guild, user,  Discord, connection) => {
+    //These are custom for 3 servers including my own. Please ignore it.
+if (guild.id === "265199259291222016" || guild.id == "303525154464727041" || guild.id === "309531752014151690") {
+    const embed = new Discord.RichEmbed()
+        .setThumbnail(user.avatarURL)
+        .setColor("#89ff89")
+        .setTitle(`${user.username} was banned.`)
+
+    guild.channels.find('name', 'mod-log').send({
+        embed
+    })
+}
+}
---
+++ events/messageDelete.js
@@ -0,0 +1,21 @@
+exports.run = (client, message, Discord, connection) => {
+    //These are custom for 3 servers including my own. Please ignore it.
+    if (message.guild.id === "265199259291222016" || message.guild.id == "303525154464727041" || message.guild.id === "309531752014151690"){
+                const embed = new Discord.RichEmbed()
+                    embed.setThumbnail(message.author.avatarURL)
+                    embed.setColor("#89ff89")
+                    embed.setTitle(`Message sent by ${message.author.username} deleted in ${message.channel.name}.`)
+                    if(message.attachments.first()){
+                        embed.addField("Image:", message.attachments.first().url)
+                    } else if(message.content.length > 0) {
+                        embed.addField("Message:", message.content)
+                    } else {
+                        embed.addField("Message:","Message was most likely an embed.")
+                    }
+                    embed.addField("Message ID:", message.id)
+                message.guild.channels.find('name', 'mod-log').send({
+                    embed
+                })
+        }
+
+    }
