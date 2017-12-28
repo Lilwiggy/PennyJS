d63701682f44a2161d4d55b2acf2fdc8c863efc9
John Wiggins <lilwiggy123@gmail.com>
2017-12-28 14:21:57 -0600

    Added //complain and //spacehuhn
    Also updated avatar command

---
+++ spacehuhn.js
@@ -0,0 +1,17 @@
+exports.run = (client, message, args, Discord) => {
+    //Astro cocks
+    var hi = Math.floor(Math.random() * 24);
+    var embed = new Discord.RichEmbed()
+    .setTitle("Here's your official Spacehuhn meme:tm:")
+    .setImage(`https://spacehuhn.de/img/gallery/${hi}.jpg`)
+    .setColor("#00b0ff")
+
+    message.channel.send({embed})
+}
+
+exports.conf = {
+    name: "spacehuhn",
+    description: "<3",
+    usage: "spacehuhn",
+    aliases: ["chicken", "stef"]
+}
