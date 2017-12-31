4ec45e43733deab817e6579da53e6ed64d88d2a7
John Wiggins <lilwiggy123@gmail.com>
2017-12-31 10:45:42 -0600

    Add files via upload

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,31 +1,42 @@
 exports.run = (client, message, args, Discord) => {
     //Fun fact: I was retarded before and forgot that I could get the avatar from the user object and made an extra API request to make myself feel special. Good times.
-    if(args.length === 1){
+    if (args.length === 1) {
         var embed = new Discord.RichEmbed()
-                .setTitle(`Your avatar`)
-                .setImage(message.author.displayAvatarURL)
-               message.channel.send(embed)
-    } else if(message.mentions.users.first()){
+        embed.setTitle(`Your avatar`)
+        if (message.author.displayAvatarURL.includes(".gif")) {
+            embed.setImage(message.author.displayAvatarURL + "&.gif")
+        } else {
+            embed.setImage(message.author.displayAvatarURL)
+        }
+        message.channel.send(embed)
+    } else if (message.mentions.users.first()) {
         var embed = new Discord.RichEmbed()
-                .setTitle(`${message.mentions.users.first().username}'s avatar`)
-                .setImage(message.mentions.users.first().displayAvatarURL)
-               message.channel.send(embed)
+        embed.setTitle(`${message.mentions.users.first().username}'s avatar`)
+        if (message.mentions.users.first().displayAvatarURL.includes(".gif")) {
+            embed.setImage(message.mentions.users.first().displayAvatarURL + "&.gif")
+        } else {
+            embed.setImage(message.mentions.users.first().displayAvatarURL)
+        }
+        message.channel.send(embed)
     } else {
-    try {
-        let user = client.users.find("username", message.content.substring(client.prefix.length + 7))
-                var embed = new Discord.RichEmbed()
-                .setTitle(`${message.content.substring(client.prefix.length + 7)}'s avatar`)
-                .setImage(user.displayAvatarURL)
-               message.channel.send(embed)
-    } catch (TypeError){
-        message.channel.send("I'm sorry, but I could not find that user.")
+        try {
+            let user = client.users.find("username", message.content.substring(client.prefix.length + 7))
+            var embed = new Discord.RichEmbed()
+            embed.setTitle(`${message.content.substring(client.prefix.length + 7)}'s avatar`)
+            if (user.displayAvatarURL.includes(".gif")) {
+                embed.setImage(user.displayAvatarURL + "&.gif");
+            } else {
+                embed.setImage(user.displayAvatarURL)
+            }
+            message.channel.send(embed)
+        } catch (TypeError) {
+            message.channel.send("I'm sorry, but I could not find that user.")
+        }
     }
-}
 }
 
 exports.conf = {
     name: "avatar",
-    category: "Miscelaneous",
     description: "Yip yip?",
     usage: "avatar",
     aliases: []
