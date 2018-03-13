7de1a9ce6808881080a5bd31a786013e9ab08b77
John Wiggins <lilwiggy123@gmail.com>
2018-03-12 23:40:19 -0500

    New welcome system

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,36 +1,38 @@
-// Oh hai Mark!
-exports.run = (client, member, Disocrd, connection) => {
-    let guild = member.guild;
-    client.checkServer(guild.id, guild.name, guild.iconURL, () => {
-        connection.query("SELECT * FROM `Servers` WHERE `ServerID` = '" + guild.id + "'", (error, results, fields) => {
-            if (error) {
-                throw new Error(error);
-            } else if (results[0].Welcome !== 0) {
-                const channel = member.guild.channels.get(results[0].wc);
-
-                if (channel) {
-                    channel.send(`**${member.user.username}** just joined **${guild.name}**. ${results[0].WMessage}`);
-                }
-            }
-        });
+exports.run = (client, member, Discord, connection) => {
+  // Oh hai Mark!
+  let guild = member.guild;
+  client.checkServer(guild.id, guild.name, guild.iconURL, () => {
+    connection.query(`SELECT * FROM \`Servers\` WHERE \`ServerID\` = '${guild.id}'`, (error, results) => {
+      if (error)
+        throw error;
+      if (results[0].Welcome === 1) {
+        const channel = member.guild.channels.get(results[0].wc);
+        if (channel) {
+          if (results[0].WMessage)
+            channel.send(`${results[0].WMessage.replace(`{user}`, member.user.username).replace(`{guild}`, guild.name)}`);
+          else
+            channel.send(`**${member.user.username}** just joined **${guild.name}**`);
+        }
+      }
     });
+  });
 
-    // This is special for now. Please ignore it. Please....
+  // This is special for now. Please ignore it. Please....
 
-    // THE DOPE FREAKING PENNY SERVER LINK IS RIGHT THE HIZZLE HERE MY MAN/WOMAN! https://discord.gg/kwcd9dq
-    if (guild.id === '309531752014151690') {
-        let role = guild.roles.find('name', 'Normies');
-        member.addRole(role);
-    } else
+  // THE DOPE FREAKING PENNY SERVER LINK IS RIGHT THE HIZZLE HERE MY MAN/WOMAN! https://discord.gg/kwcd9dq
+  if (guild.id === '309531752014151690') {
+    let role = guild.roles.find('name', 'Normies');
+    member.addRole(role);
+  } else
 
-    // Sky's server
-    if (guild.id === '303525154464727041') {
-        let role = guild.roles.find('name', 'Users');
-        member.addRole(role);
-    }
-    // PivotsXXD server
-    if (guild.id === '346800980102348810') {
-        let role = guild.roles.find('name', 'Pivoteer');
-        member.addRole(role);
-    }
+  // Sky's server
+  if (guild.id === '303525154464727041') {
+    let role = guild.roles.find('name', 'Users');
+    member.addRole(role);
+  }
+  // PivotsXXD server
+  if (guild.id === '346800980102348810') {
+    let role = guild.roles.find('name', 'Pivoteer');
+    member.addRole(role);
+  }
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,17 +1,19 @@
-// Should I stay or should I g- oh they left already...
 exports.run = (client, member, Discord, connection) => {
-    let guild = member.guild;
-    client.checkServer(guild.id, guild.name, guild.iconURL, () => {
-        connection.query("SELECT * FROM `Servers` WHERE `ServerID` = '" + guild.id + "'", (error, results, fields) => {
-            if (error) {
-                throw new Error(error);
-            }
-            if (results[0].Welcome !== 0) {
-                const channel = member.guild.channels.get(results[0].wc);
-                if (channel) {
-                    channel.send(`**${member.user.username}** just left **${guild.name}**. ${results[0].LMessage}`);
-                }
-            }
-        });
+  // Should I stay or should I g- oh they left already...
+  let guild = member.guild;
+  client.checkServer(guild.id, guild.name, guild.iconURL, () => {
+    connection.query(`SELECT * FROM \`Servers\` WHERE \`ServerID\` = '${guild.id}'`, (error, results) => {
+      if (error)
+        throw error;
+      if (results[0].Welcome === 1) {
+        const channel = member.guild.channels.get(results[0].wc);
+        if (channel) {
+          if (results[0].LMessage)
+            channel.send(`${results[0].LMessage.replace(`{user}`, member.user.username).replace(`{guild}`, guild.name)}`);
+          else
+            channel.send(`**${member.user.username}** just left **${guild.name}**`);
+        }
+      }
     });
+  });
 };
