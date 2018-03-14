6ace4e988548162addd9f425749569bc9f9e362c
John Wiggins <lilwiggy123@gmail.com>
2018-03-14 09:59:51 -0500

    Bug fix

--- commands/define.js
+++ commands/define.js
@@ -1,44 +1,44 @@
+exports.run = (client, message, args, Discord) => {
 // Search doctorbating on urban dictionary
-exports.run = (client, message, args, Discord, connection) => {
-    if (args.length === 1) {
-        message.channel.send('Usage: //define [Term to define]');
-    } else {
-        let http = require('http');
-        let url = `http://api.urbandictionary.com/v0/define?term=${message.content.substring(client.prefix.length + 6)}`;
+  if (args.length === 1) {
+    message.channel.send(`Usage: ${client.prefix}define [Term to define]`);
+  } else {
+    var http = require('http');
+    var url = `http://api.urbandictionary.com/v0/define?term=${message.content.substring(client.prefix.length + 6)}`;
+    http.get(url, (res) => {
+      var body = '';
 
-        http.get(url, (res) => {
-            let body = '';
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
 
-            res.on('data', (chunk) => {
-                body += chunk;
-            });
-
-            res.on('end', () => {
-                let data = JSON.parse(body);
-
-                if (data.result_type !== 'no_results') {
-                    if (data.list[0].definition.length > 1024) {
-                        message.channel.send("I'm sorry but the definition is too long for me to put in an embed.");
-                    } else {
-                        let embed = new Discord.RichEmbed()
-                        .setColor('#89ff89')
-                        .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL)
-                        .addField(`Definition for: ${message.content.substring(client.prefix.length + 6)}`, data.list[0].definition);
-
-                        message.channel.send(embed);
-                    }
-                } else {
-                    message.channel.send("It seems that word doesn't have a definition.");
-                }
-            });
-        });
-    }
+      res.on('end', () => {
+        try {
+          let data = JSON.parse(body);
+          if (data.result_type !== 'no_results') {
+            if (data.list[0].definition.length > 1024) {
+              message.channel.send("I'm sorry but the definition is too long for me to put in an embed.");
+            } else {
+              var embed = new Discord.RichEmbed()
+                .setColor('#89ff89')
+                .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL)
+                .addField(`Definition for: ${message.content.substring(client.prefix.length + 6)}`, data.list[0].definition);
+              message.channel.send(embed);
+            }
+          } else {
+            message.channel.send("It seems that word doesn't have a definition.");
+          }
+        } catch (SyntaxError) {
+          message.channel.send(`Sorry something went wrong. This usually happens when people use the ‘ character. Please don't do that.`);
+        }
+      });
+    });
+  }
 };
 
 exports.conf = {
-    name: 'define',
-    category: 'Miscelaneous',
-    description: 'Defines, things',
-    usage: 'define',
-    aliases: [],
+  name: 'define',
+  description: 'Defines, things',
+  usage: 'define',
+  aliases: [],
 };
