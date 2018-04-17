96ab2846dd490fe55564352de9096de4174570fd
John Wiggins <lilwiggy123@gmail.com>
2018-04-17 14:09:21 -0500

    Fixed no such table user bug

--- commands/weeb.js
+++ commands/weeb.js
@@ -2,10 +2,10 @@ exports.run = (client, message, args, Discord, connection) => {
   if (args[1]) {
     client.checkUser(message.author.id, message.author.displayAvatarURL, () => {
       if (args[1] === 'on') {
-        connection.query(`UPDATE \`user\` SET \`weeb\` = 'on' WHERE \`User_ID\` = ${message.author.id}`);
+        connection.query(`UPDATE \`User\` SET \`weeb\` = 'on' WHERE \`User_ID\` = ${message.author.id}`);
         message.channel.send('You will now receive anime images from the hug command.');
       } else if (args[1] === 'off') {
-        connection.query(`UPDATE \`user\` SET \`weeb\` = 'off' WHERE \`User_ID\` = ${message.author.id}`);
+        connection.query(`UPDATE \`User\` SET \`weeb\` = 'off' WHERE \`User_ID\` = ${message.author.id}`);
         message.channel.send('You will no longer receive anime images from the hug command.');
       } else {
         message.channel.send(`Usage: ${client.prefix}weeb [on/off]`);
