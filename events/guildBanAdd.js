49da5d1f4c7768876fa2bc80a9ab36ab6b642739
John Wiggins <lilwiggy123@gmail.com>
2018-05-05 22:17:26 -0500

    Banned m8 got annoying in the logs

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,27 +1,28 @@
 exports.run = (client, guild, user, Discord, connection) => {
   // These are custom for 3 servers including my own. Please ignore it.
-  console.log(`Banned m8`);
   client.checkServer(guild.id, guild.name, guild.iconURL, () => {
     connection.query(`SELECT * FROM \`Servers\` WHERE \`ServerID\` = '${guild.id}'`, (error, results) => {
       if (results[0].mod_log === 1) {
-        let channel = guild.channels.get(results[0].mod_channel);
-        guild.fetchAuditLogs()
-          .then((audit) => {
-            let audits = audit.entries.first();
-            if (audits.target.id === user.id) {
-              let embed = new Discord.RichEmbed()
-                .setThumbnail(user.displayAvatarURL)
-                .setColor('#f42424')
-                .setTitle(`New ban.`)
-                .addField(`New ban added.`, `${audits.target.username} was banned by ${audits.executor.username}.`);
-              if (audits.reason)
-                embed.addField(`Reason:`, audits.reason);
-              else
-                embed.addField(`Reason:`, `None provided.`);
-              if (channel)
-                channel.send({ embed });
-            }
-          });
+        setTimeout(() => {
+          let channel = guild.channels.get(results[0].mod_channel);
+          guild.fetchAuditLogs()
+            .then((audit) => {
+              let audits = audit.entries.first();
+              if (audits.target.id === user.id) {
+                let embed = new Discord.RichEmbed()
+                  .setThumbnail(user.displayAvatarURL)
+                  .setColor('#f42424')
+                  .setTitle(`New ban.`)
+                  .addField(`New ban added.`, `${audits.target.username} was banned by ${audits.executor.username}.`);
+                if (audits.reason)
+                  embed.addField(`Reason:`, audits.reason);
+                else
+                  embed.addField(`Reason:`, `None provided.`);
+                if (channel)
+                  channel.send({ embed });
+              }
+            });
+        }, 1000);
       }
     });
   });
