41eb4cc8f34374afef35387dd2719f8f968819b6
John Wiggins <lilwiggy123@gmail.com>
2018-05-07 20:41:50 -0500

    *oops*
    Forgot to check if channel was NSFW

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,10 +1,12 @@
 exports.run = (client, message) => {
   // I know what you're doing here ( ͡° ͜ʖ ͡°)
-
-  let images = require(`../images.json`);
-  let response = images.nsfw[Math.floor(Math.random() * images.nsfw.length)];
-
-  message.channel.send({ file: response });
+  if (!message.channel.nsfw) {
+    message.channel.send(`This channel is not marked as NSFW.`);
+  } else {
+    let images = require(`../images.json`);
+    let response = images.nsfw[Math.floor(Math.random() * images.nsfw.length)];
+    message.channel.send({ file: response });
+  }
 };
 
 exports.conf = {
