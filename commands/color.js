b9715b672d294cb66e5cd0e10cc4c74f0eca6c7e
John Wiggins <lilwiggy123@gmail.com>
2018-05-17 16:39:33 -0500

    Make this  thing but better

--- commands/color.js
+++ commands/color.js
@@ -1,50 +1,39 @@
-// Credit:
-// Command by Uninvited - Uninvited#1307 on Discord
+exports.run = (client, message, args, Discord) => {
+  const Canvas = require('canvas');
+  const hex = /^#?[0-9A-F]{6}$/i;
+  if (message.mentions.members.first()) {
+    message.channel.send(`${message.mentions.users.first().username}'s color is **${message.mentions.members.first().displayHexColor}**`);
+  } else if (args.length > 1) {
+    if (hex.test(args[1])) {
+      message.channel.startTyping();
+    let Image = Canvas.Image;
+    let canvas = new Canvas(100, 100);
+    let ctx = canvas.getContext('2d');
 
-// This command is for the guild of the bot for patrons to add a custom hex as
-// a patreon perk.
-exports.run = (client, message, args) => {
-  // Function to check if user has color and remove color to assign a new one,
-  // this part will execute even if you are not a patreon but will in turn just
-  // remove the role and fail at setcolor()
-  if (message.guild.roles.exists('name', message.author.id)) {
-    let roleee = message.guild.roles.find('name', message.author.id);
-    roleee.edit({
-      color: args[1],
+    let img = new Image();
+    if (args[1].includes('#'))
+    ctx.fillStyle = args[1];
+    else
+    ctx.fillStyle = `#${args[1]}`;
+    ctx.fillRect(0, 0, 1920, 1080);
+    canvas.toBuffer((e, buff) => {
+      let att = new Discord.Attachment()
+          .setAttachment(buff, `${args[1]}.png`);
+          message.channel.send(`Color for **${args[1]}:**`, { file: att }).then((msg) => {
+            msg.channel.stopTyping();
+          });
     });
-    message.channel.send(`Enjoy your hex!`);
+    } else {
+      message.channel.send(`Please input a proper hex color.`);
+    }
   } else {
-    setcolor(message, args);
+    message.channel.send(`Your color color is **${message.member.displayHexColor}**`);
   }
 };
 
 exports.conf = {
   name: 'color',
   description: 'Color',
-  usage: 'color (hex)',
+  usage: 'color [color]/[@user]',
   aliases: ['colour'], // Some British asshole told me to add this.
-  hidden: true,
 };
-
-// function to check if user is patreon and assign color
-function setcolor(message, args) {
-  if (message.guild.id === '309531752014151690') {
-    if (message.member.roles.get('318256979497975810')) {
-      let isOk = /^#[0-9A-F]{6}$/i;
-      if (isOk.test(args[1])) {
-        message.member.guild.createRole({
-          name: message.author.id,
-          color: args[1],
-        }).then((role) => {
-          role.setPosition(message.guild.roles.get('438212196309401611').position + 1);
-          message.member.addRole(role);
-          message.channel.send('Enjoy your hex!');
-        });
-      } else {
-        message.channel.send('Please use a valid hex.');
-      }
-    } else {
-      message.channel.send('This feature is for Patreons only.');
-    }
-  }
-}
