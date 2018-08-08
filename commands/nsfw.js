68daabf28fe8d20150dcb3377e643e594e1ba1f7
John Wiggins <lilwiggy123@gmail.com>
2018-08-08 11:49:18 -0500

    Better random images

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -4,11 +4,40 @@ exports.run = (client, message) => {
     message.channel.send(`This channel is not marked as NSFW.`);
   } else {
     let images = require(`../images.json`);
-    let response = images.nsfw[Math.floor(Math.random() * images.nsfw.length)];
-    message.channel.send({ file: response });
+    if (!client.guilds.get(message.guild.id).nsfwArr)
+      client.guilds.get(message.guild.id).nsfwArr = [];
+    message.channel.send({ file: randomImage(client.guilds.get(message.guild.id).nsfwArr, images.nsfw) });
   }
 };
 
+function randomImage(arr, images) {
+  let ran = images[Math.floor(Math.random() * images.length)];
+  if (arr.length === images.length) {
+    arr = [];
+    return ran;
+  } else {
+    shuffle(arr);
+    if (arr.includes(ran)) {
+      randomImage(arr, images);
+    } else {
+      arr.push(ran);
+      return ran;
+    }
+  }
+}
+
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+// <3
+function shuffle(a) {
+  let j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
 exports.conf = {
   name: 'nsfw',
   description: '( ͡° ͜ʖ ͡°)',
--- commands/waifu.js
+++ commands/waifu.js
@@ -1,15 +1,39 @@
-exports.run = (client, message, args, Discord) => {
+exports.run = (client, message) => {
   // This command is best girl. Just sayin'
-
   let images = require(`../images.json`);
-  let response = images.waifu[Math.floor(Math.random() * images.waifu.length)];
-
-  var embed = new Discord.RichEmbed()
-    .setColor('#89ff89')
-    .setTitle('I approve.')
-    .setImage(response);
-  message.channel.send({ embed });
+  if (!client.guilds.get(message.guild.id).waifuArr)
+    client.guilds.get(message.guild.id).waifuArr = [];
+  message.channel.send(`I approve.`, { file: randomImage(client.guilds.get(message.guild.id).waifuArr, images.waifu) });
 };
+
+function randomImage(arr, images) {
+  let ran = images[Math.floor(Math.random() * images.length)];
+  if (arr.length === images.length) {
+    arr = [];
+    return ran;
+  } else {
+    shuffle(arr);
+    if (arr.includes(ran)) {
+      randomImage(arr, images);
+    } else {
+      arr.push(ran);
+      return ran;
+    }
+  }
+}
+
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+// <3
+function shuffle(a) {
+  let j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
 
 exports.conf = {
   name: 'waifu',
