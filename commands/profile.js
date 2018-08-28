f8dc767a8bf8d3225b9363e36abfcfb89d47f00c
John Wiggins <lilwiggy123@gmail.com>
2018-08-28 13:10:54 -0500

    Clean up
    Also changed a bit so you can use a username instead

--- commands/profile.js
+++ commands/profile.js
@@ -10,38 +10,38 @@ exports.run = (client, message, args, Discord, connection) => {
   // Anti spam bit
   let userID = message.author.id;
   let command = 'profile';
+  let user = message.mentions.users.first() || message.guild.members.find((m) => m.user.username === args[1]) || message.author;
+  if (user.user)
+    user = user.user;
   connection.query(`SELECT COUNT(*) as \`count\`, \`User_ID\` FROM \`usercool\` WHERE \`User_ID\` = ${userID}`, (err, res) => {
     if (err)
       throw err;
     if (res[0].count === 0) {
       connection.query(`INSERT INTO \`usercool\`(\`User_ID\`, \`command\`, \`cool\`) VALUES (${userID}, 'profile' , NOW())`);
-      let user = message.mentions.users.first() || message.author
-        if (user.bot) {
-          // No robot uprising today!
-          message.channel.send("Haha. Silly you. Bots don't have profiles!");
-        } else {
-          // Good naming system, idiot.
-          pro.pro(client, user.id, user.displayName, user.displayAvatarURL, message, connection, Discord);
-        }
+      if (user.bot) {
+        // No robot uprising today!
+        message.channel.send("Haha. Silly you. Bots don't have profiles!");
+      } else {
+        // Good naming system, idiot.
+        pro.pro(client, user.id, user.username, user.displayAvatarURL, message, connection, Discord);
+      }
     } else {
       connection.query(`SELECT *,NOW()-INTERVAL 1 MINUTE > \`cool\` AS cooldown,(TO_SECONDS(\`cool\`)-TO_SECONDS(NOW() - INTERVAL 1 MINUTE)) AS restTime, NOW()  FROM \`usercool\` WHERE \`User_ID\`='${userID}' AND \`command\` = '${command}'`, (err1, res1, fields1) => {
         if (res1[0].cooldown === 0) {
           connection.query(`UPDATE \`usercool\` SET \`cool\` =NOW() WHERE \`command\` = '${command}' AND \`User_ID\` = '${userID}'`);
-           let user = message.mentions.users.first() || message.member
-            if (user.bot) {
-              // Still preventing the robot uprising here^^
-              message.channel.send("Haha. Silly you. Bots don't have profiles!");
-            } else {
-              pro.pro(client, user.id, user.displayName, user.displayAvatarURL, message, connection, Discord);
-            }
+          if (user.bot) {
+            // Still preventing the robot uprising here^^
+            message.channel.send("Haha. Silly you. Bots don't have profiles!");
+          } else {
+            pro.pro(client, user.id, user.username, user.displayAvatarURL, message, connection, Discord);
+          }
         } else {
           connection.query(`UPDATE \`usercool\` SET \`cool\` =NOW() WHERE \`command\` = '${command}' AND \`User_ID\` = '${userID}'`);
-           let user = message.mentions.users.first() || message.author
-            if (user.bot) {
-              // Still preventing the robot uprising here^^
-              message.channel.send("Haha. Silly you. Bots don't have profiles!");
-            } else {
-              pro.pro(client, user.id, user.displayName, user.displayAvatarURL, message, connection, Discord);
+          if (user.bot) {
+            // Still preventing the robot uprising here^^
+            message.channel.send("Haha. Silly you. Bots don't have profiles!");
+          } else {
+            pro.pro(client, user.id, user.username, user.displayAvatarURL, message, connection, Discord);
           }
         }
       });
