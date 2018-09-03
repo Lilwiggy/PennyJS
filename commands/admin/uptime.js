6bd5604ea2e2e98e22d3f7650aab39103700637b
John Wiggins <lilwiggy123@gmail.com>
2018-09-03 12:51:15 -0500

    Oops

--- commands/admin/uptime.js
+++ commands/admin/uptime.js
@@ -3,13 +3,13 @@ exports.run = (client, message) => {
 // Shout out to https://github.com/Gravebot/Gravebot/blob/master/src/commands/info/uptime.js for
 // doing the math I didn't wanna do
   let days = Math.floor(process.uptime() / (60 * 60 * 24));
-  let hours = Math.floor(process.uptime() / (60 * 60));
+  let hours = Math.floor(process.uptime() / (60 * 60) % 24);
   let minutes = Math.floor(process.uptime() % (60 * 60) / 60);
   let seconds = Math.floor(process.uptime() % 60);
   if (message)
     message.channel.send(`My uptime: ${days} days ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`);
   else
-    return `My uptime: ${days} days ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`;
+    return `${days} days ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`;
 };
 
 
