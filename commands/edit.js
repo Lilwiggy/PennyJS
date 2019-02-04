34b2a8a1e23d87bdcc3f286ab7439851155a02f3
John Wiggins <lilwiggy123@gmail.com>
2019-02-04 17:47:02 -0600

    Confirmation image for edit command

--- commands/edit.js
+++ commands/edit.js
@@ -1,8 +1,25 @@
 exports.run = (client, message, args, Discord, connection) => {
+  const Canvas = require('canvas');
+  const neko = require('nekocurl');
   let role = message.content.slice(client.prefix.length + args[0].length).split(' | ');
   let r_id;
   let edit = true;
   let blacklisted = [];
+  let terms = [
+    'Really? That\'s what my new color is?',
+    'Are you freaking kissing me rn?',
+    'You wanna hear it purr?',
+    'Go commit smh',
+    'My kitty is purring so fast rn',
+    'It\'s ya boi, uh skinny penis',
+    'Coulda picked a better color tbhtbh',
+    'Literally kiss me on the pecker',
+    'you\'re mom gay lol',
+    'All I did was have sushi with my mom',
+    'I\'m trying to mimick my cat rn',
+    'Stop sitting on my tiddy >:(',
+    `Honestly I only joined for the hentai chat`,
+  ];
   client.checkServer(message.guild.id, message.guild.name, message.guild.iconURL, () => {
     connection.query(`SELECT * FROM \`Servers\` WHERE \`ServerID\` = ${message.guild.id}`, (err, guild) => {
       if (err)
@@ -31,8 +48,59 @@ exports.run = (client, message, args, Discord, connection) => {
                   if (hexThing.test(role[1])) {
                     let rtu = memberRole;
                     if (rtu.editable) {
-                      rtu.setColor(role[1], `Color change requested by ${message.author.username}`);
-                      message.channel.send(`Your role color is now ${role[1]}. Enjoy!`);
+                      let term = terms[Math.floor(Math.random() * terms.length)];
+                      let canvas = new Canvas(term.length * 9 + 100, 100);
+                      let Image = Canvas.Image;
+                      let ctx = canvas.getContext('2d');
+                      let img = new Image();
+                      ctx.fillStyle = '#36393f';
+                      ctx.fillRect(0, 0, term.length * 9 + 100, 100);
+                      ctx.font = `16px "Whitney Helvetica Neue"`;
+                      ctx.fillStyle = '#fff';
+                      ctx.fillText(term, 75, 70);
+                      ctx.font = `20px "Whitney Helvetica Neue"`;
+                      ctx.fillStyle = role[1];
+                      ctx.fillText(message.member.displayName, 75, 45);
+                      async function render() {
+                        img.src = await neko.get(message.author.displayAvatarURL, { autoString: false });
+                        ctx.save();
+
+                        ctx.beginPath();
+                        ctx.arc(5 + (60 / 2), 25 + (60 / 2), 60 / 2, 0, Math.PI * 2, false);
+
+                        ctx.clip();
+                        ctx.drawImage(img, 5, 25, 60, 60);
+                        ctx.restore();
+                        canvas.toBuffer((e, buff) => {
+                          if (e)
+                            client.users.get(`232614905533038593`).send(`Error:\n${e}`);
+                          let pro = new Discord.Attachment()
+                            .setAttachment(buff, `color.png`);
+
+                          message.channel.send(`This is a preview of your new color. Are you sure you want to keep it?`, {
+                            file: pro,
+                          }).then((m) => {
+                            m.react(`🇾`).then((r) => {
+                              r.message.react(`🇳`);
+                            });
+
+                            const filter = (r, user) => user.id === message.author.id;
+                            const collector = m.createReactionCollector(filter, { time: 60000 * 2 });
+
+                            collector.on(`collect`, (r) => {
+                              r.remove(message.author);
+                              if (r.emoji.name === `🇾`) {
+                                rtu.setColor(role[1], `Color change requested by ${message.author.username}`);
+                                message.channel.send(`Your role color is now ${role[1]}. Enjoy!`);
+                              } else if (r.emoji.name === `🇳`) {
+                                message.channel.send(`I will not update your role to this color. To try something better edit your message with your new hex.`);
+                              }
+                              collector.stop();
+                            });
+                          });
+                        });
+                      }
+                      render();
                     } else {
                       message.channel.send(`Coud not edit role. Missing permissions.`);
                     }
