64b694b1401bfc81c7bcdb807df0f1e60040bdae
John Wiggins <lilwiggy123@gmail.com>
2019-08-10 00:51:14 -0500

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -11,17 +11,18 @@ exports.run = (client, message, Discord, connection) => {
 
   // Custom prefix things
   client.checkServer(message.guild.id, message.guild.name, message.guild.iconURL, () => {
-    connection.query(`SELECT * FROM \`Servers\` WHERE \`ServerID\` = '${message.guild.id}'`, (error, prefix) => {
+    connection.query(`SELECT \`Prefix\`, \`levels\` FROM \`Servers\` WHERE \`ServerID\` = '${message.guild.id}'`, (error, prefix) => {
+      client.prefix = prefix[0].Prefix; // set that prefix boi
       client.checkUser(message.author.id, message.author.displayAvatarURL, () => {
         connection.query(`SELECT * FROM \`User\` WHERE \`User_ID\` = ${message.author.id}`, (err, res) => {
           if (err)
             throw err;
-            xpAdd(connection, message, prefix[0].levels);
           try {
             // This is for seeing if the user is blacklisted or not. Some people man smh.
             if (res[0].Blacklisted === 1) {
               return;
             } else {
+              xpAdd(connection, message, prefix[0].levels);
               // This uh, uhm, yeah it does that one thing where it ignores things with some other bits.
               if (message.content.indexOf(client.prefix) !== 0)
                 return;
@@ -39,12 +40,24 @@ exports.run = (client, message, Discord, connection) => {
                 }
               } else {
                 connection.query(`SELECT * FROM \`tags\` WHERE \`guild\` = ${message.guild.id} AND \`name\` = ${connection.escape(command[0])}`, (e, tag) => {
-                  if (e)
+                  if (e) {
                     client.users.get('232614905533038593').send(`Error:\n${e}\nUsed in:\n${message.content}`);
+                  }
                   if (tag.length > 0) {
                     console.log(`Ran tag: ${command[0]} by user ${message.author.username}\nUser ID: ${message.author.id}`);
                     connection.query(`UPDATE \`tags\` SET \`used\` = \`used\` + 1 WHERE \`name\` = ${connection.escape(command[0])} AND \`guild\` = ${message.guild.id}`);
-                    message.channel.send(clean(tag[0].content));
+                    let s = tag[0].content.replace(/{username}/g, message.author.username);
+                    if (/{mentions.username}/g.test(s)) {
+                      if (!message.mentions.users.first()) {
+                        message.channel.send(`This tag requires you mention a user!`);
+                        return;
+                      }
+                      s = s.replace(/{mentions.username}/g, message.mentions.users.first().username);
+                    }
+                    // I do it like this so mentioning people still works in tags
+                    s = s.replace(/@everyone/g, 'everyone');
+                    s = s.replace(/@here/g, 'here');
+                    message.channel.send(s);
                   }
                 });
               }
@@ -53,8 +66,6 @@ exports.run = (client, message, Discord, connection) => {
             console.log('Hi'); // Somehow this works trust me.
           }
         });
-
-        client.prefix = prefix[0].Prefix; // set that prefix boi
 
         // Gotta eay at argsbys *funny right?* Seriosuly tho, eat Arby's the food is fantastic and they make good memes on twitter
         const args = message.content.slice(client.prefix.length).split(' ');
@@ -106,7 +117,7 @@ function xpAdd(connection, message, enabled) {
     connection.query(`SELECT *,NOW()-INTERVAL 2 MINUTE > \`xp_cool\` AS xpAdd FROM \`User\` WHERE \`User_ID\` = '${message.author.id}'`, (err1, res1) => {
       if (err1)
         throw err1;
-      if (res1[0].xpAdd === 1) {
+      if (res1[0].xpAdd === 1 || res1[0].xpAdd === null || res1[0].xpAdd === undefined) {
         var xp = [Math.floor(Math.random() * 50)]; // 50 xp max at random. Just to make leveling up hard as balls
         connection.query(`UPDATE \`User\` SET \`xp_cool\`=NOW(), \`XP\`=\`XP\` + '${xp}' WHERE \`User_ID\` = '${message.author.id}'`);
         if (res[0].XP > res[0].Next) {
