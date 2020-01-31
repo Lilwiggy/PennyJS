97496c3efd652eaaa5ffb8a23c948507c7dd886c
John Wiggins <lilwiggy123@gmail.com>
2020-01-30 22:44:18 -0600

    Fix listening command

--- commands/listening.js
+++ commands/listening.js
@@ -1,12 +1,95 @@
-exports.run = () => {
-//  Find the spotify.js file for this command.
-// This is blank as discord.js doesn't have rich presence support.
+/* eslint-disable linebreak-style */
+exports.run = (client, message) => {
+//  Please note I have edited the base Presece structure in discord.js as to allow
+// for custom emoji support while listening to music. I know it sounds stupid but
+// it wouldn't work before due to how d.js handled activties.
+	let member = client.fetchGuildMember(message) || message.member;
+	if (!member) {
+		message.channel.send('I cannot find that user.');
+	} else {
+		let pre = member.presence.game;
+		if (!pre.filter((g) => g.type === 2 && g.name === 'Spotify')) {
+			message.channel.send('You aren\'t listening to anything.');
+		} else {
+			pre = pre.find((g) => g.type === 2 && g.name === 'Spotify');
+			console.log(pre);
+			let album = [];
+			let artist = [];
+			let listening = [];
+			let users = message.guild.members.filter((m) => {
+				if (m.bot)
+					return;
+				if (m.id === member.id)
+					return;
+				let mPresence = m.presence.game.find((g) => g.type === 2 && g.name === 'Spotify');
+				if (mPresence) {
+					if (mPresence.details === pre.details) {
+						listening.push(m);
+						return mPresence.details === pre.details;
+					} else if (mPresence.assets.large_text === pre.assets.large_text) {
+						album.push(m);
+						return mPresence.assets.large_text === pre.assets.large_text;
+					} else if (mPresence.state === pre.state) {
+						artist.push(m);
+						return mPresence.state === pre.state;
+					}
+				}
+			});
+			let embed = {
+				title: pre.details,
+				thumbnail: {
+					url: `https://i.scdn.co/image/${pre.assets.large_image.substr(8)}`,
+				},
+				color: 2021216,
+				description: `By: ${pre.state}\nAlbum: ${pre.assets.large_text}`,
+				fields: [
+				],
+			};
+			if (users.size > 0) {
+				let listeners = [];
+				let albums = [];
+				let artists = [];
+				users.forEach((u) => {
+					let uname = u.user.username;
+					if (u.id === '120308435639074816') 
+						uname = 'RaidAndGay';
+
+					if (listening.includes(u))
+						listeners.push(uname);
+					else if (album.includes(u))
+						albums.push(uname);
+					else if (artist.includes(u))
+						artists.push(uname);
+				});
+
+				if (listening.length > 0) {
+					if (listening.length === 1)
+						embed.fields.push({ name: `Listening with ${listening.length} other.`, value: listeners.toString() });
+					else
+						embed.fields.push({ name: `Listening with ${listening.length} others.`, value: listeners.toString() });
+				} else if (album.length > 0) {
+					if (album.length > 1)
+						embed.fields.push({ name: `${album.length} others are listening to this album.`, value: albums.join(', ').toString() });
+					else
+						embed.fields.push({ name: `${album.length} other is listening to this album.`, value: albums.toString() });
+				}
+				if (artist.length > 0) {
+					if (artist.length === 1)
+						embed.fields.push({ name: `${artist.length} other is listening to this artist.`, value: artists.toString() });
+					else
+						embed.fields.push({ name: `${artist.length} others are listening to this artist.`, value: artists.join(', ').toString() });
+				}
+			}
+
+			message.channel.send({ embed: embed });
+		}
+	}
 };
 
 
 exports.conf = {
-  name: 'listening',
-  description: 'Shows what you\'re listening to on Spotify.',
-  usage: 'listening',
-  aliases: [],
+	name: 'listening',
+	description: 'Shows what you\'re listening to on Spotify.',
+	usage: 'listening',
+	aliases: [],
 };
--- modules/functions.js
+++ modules/functions.js
@@ -1,149 +1,188 @@
-module.exports = (client, connection) => {
-  // Super secret spy stuff that I took "inspiration" from An Idiot's guide for. It just loads commands and stuff, I made it a little different. DON'T SHAME ME!
-  client.loadCommand = (commandName) => {
-    try {
-      const props = require(`../commands/${commandName}`);
-      if (props.init)
-        props.init(client);
+module.exports = (client, connection, Raven) => {
+	// Super secret spy stuff that I took "inspiration"
+	// from An Idiot's guide for. It just loads commands and stuff, I made it a little different. DON'T SHAME ME!
 
-      client.commands.set(props.conf.name, props);
-      props.conf.aliases.forEach((alias) => {
-        client.aliases.set(alias, props.conf.name);
-      });
-      return false;
-    } catch (e) {
-      return `Unable to load command ${commandName}: ${e}`;
-    }
-  };
+	client.error = (e) => {
+		Raven.captureException(e);
+	};
+	client.loadCommand = (commandName) => {
+		try {
+			const props = require(`../commands/${commandName}`);
+			if (props.init)
+				props.init(client);
 
-  // Admin commands
-  client.loadAdminCommand = (commandName) => {
-    try {
-      const props = require(`../commands/admin/${commandName}`);
-      if (props.init)
-        props.init(client);
+			client.commands.set(props.conf.name, props);
+			props.conf.aliases.forEach((alias) => {
+				client.aliases.set(alias, props.conf.name);
+			});
+			return false;
+		} catch (e) {
+			return `Unable to load command ${commandName}: ${e}`;
+		}
+	};
 
-      client.adminCommands.set(props.conf.name, props);
-      return false;
-    } catch (e) {
-      return `Unable to load command ${commandName}: ${e}`;
-    }
-  };
+	// Admin commands
+	client.loadAdminCommand = (commandName) => {
+		try {
+			const props = require(`../commands/admin/${commandName}`);
+			if (props.init)
+				props.init(client);
 
-  // Check to see if the user is in the database.
-  client.checkUser = (userID, UserAvatar, callback) => {
-    connection.query(`SELECT COUNT(*) AS \`count\`, \`UserAvatar\` FROM \`User\` WHERE \`User_ID\`='${userID}'`, (error, results) => {
-      if (results[0].count === 0) {
-        connection.query(`INSERT INTO \`User\`(\`User_ID\`) VALUES ('${userID}')`, (err, res) => {
-          if (err)
-            throw err;
-          callback();
-        });
-      } else {
-        if (results[0].UserAvatar !== UserAvatar)
-          connection.query(`UPDATE \`User\` SET \`UserAvatar\` = '${UserAvatar}' WHERE \`User_ID\`='${userID}'`, (err1, res1) => {});
-        callback();
-      }
-    });
-  };
+			client.adminCommands.set(props.conf.name, props);
+			return false;
+		} catch (e) {
+			return `Unable to load command ${commandName}: ${e}`;
+		}
+	};
 
-
-  // But is the guild in the db?
-  client.checkServer = (serverID, serverName, serverIcon, callback) => {
-    connection.query(`SELECT COUNT(*) AS \`count\`, \`ServerIcon\`,\`ServerName\` FROM \`Servers\` WHERE \`ServerID\` ='${serverID}'`, (error, results) => {
-      if (results[0].count === 0) {
-        connection.query(`INSERT INTO \`Servers\`(\`ServerID\`, \`ServerName\`) VALUES ('${serverID}', ${connection.escape(serverName)})`, (error, results) => {
-          callback();
-        });
-      } else {
-        if (results[0].ServerIcon !== serverIcon)
-          connection.query(`UPDATE \`Servers\` SET \`ServerIcon\` = '${serverIcon}' WHERE \`ServerID\` = '${serverID}'`);
-
-        if (results[0].ServerName !== serverName)
-          connection.query(`UPDATE \`Servers\` SET \`ServerName\` = ${connection.escape(serverName)} WHERE \`ServerID\` = '${serverID}'`);
-
-        callback();
-      }
-    });
-  };
+	// Check to see if the user is in the database.
+	client.checkUser = (userID, UserAvatar, callback) => {
+		try {
+			connection.query(`SELECT COUNT(*) AS \`count\`, \`UserAvatar\` FROM \`User\` WHERE \`User_ID\`='${userID}'`, (error, results) => {
+				if (results[0].count === 0) {
+					connection.query(`INSERT INTO \`User\`(\`User_ID\`, \`UserAvatar\`, \`Used\`) VALUES ('${userID}', '${UserAvatar}', 0)`, (err) => {
+						if (err)
+							throw err;
+						callback();
+					});
+				} else {
+					if (results[0].UserAvatar !== UserAvatar)
+						connection.query(`UPDATE \`User\` SET \`UserAvatar\` = '${UserAvatar}' WHERE \`User_ID\`='${userID}'`, () => {});
+					callback();
+				}
+			});
+		} catch (e) {
+			client.error(e);
+		}
+	};
 
 
-  // Trust me you're gonna wanna add this.
+	// But is the guild in the db?
+	client.checkServer = (serverID, serverName, serverIcon, callback) => {
+		try {
+			connection.query(`SELECT COUNT(*) AS \`count\`, \`ServerIcon\` FROM \`Servers\` WHERE \`ServerID\` = '${serverID}'`, (error, results) => {
+				if (error)
+					console.log(error);
+				if (results[0].count === 0) {
+					console.log(serverID);
+					connection.query(`INSERT INTO \`Servers\` (\`ServerID\`) VALUES ('${serverID}')`, (error) => {
+						if (error)
+							console.log(error);
+						callback();
+					});
+				} else {
+					if (results[0].ServerIcon !== serverIcon)
+						connection.query(`UPDATE \`Servers\` SET \`ServerIcon\` = '${serverIcon}' WHERE \`ServerID\` = '${serverID}'`);
+					callback();
+				}
+			});
+		} catch (e) {
+			console.log(e);
+			client.error(e);
+		}
+	};
 
-  client.setBackground = (id, avatar, background, amount, msg) => {
-    client.checkUser(id, avatar, () => {
-      connection.query(`SELECT COUNT(*) AS hasB FROM \`userB\` WHERE \`User_ID\` = ${id} AND \`name\` = '${background}'`, (err1, res1, fields1) => {
-        if (res1[0].hasB === 0) {
-          connection.query(`SELECT *  FROM \`User\` WHERE \`User_ID\` = ${id}`, (err, res, fields) => {
-            if (err)
-              throw err;
-            if (res[0].Credits < amount) {
-              msg.channel.send("I'm sorry you don't have enough money for this background.");
-            } else {
-              connection.query(`UPDATE \`User\` SET \`background\` = '${background}', \`Credits\`=\`Credits\` -${amount} WHERE \`User_ID\` = ${id}`);
-              connection.query(`INSERT INTO \`userB\` (\`User_ID\`, \`name\`) VALUES (${id}, '${background}')`);
-              msg.channel.send('Successfully bought your background.');
-            }
-          });
-        } else {
-          connection.query(`UPDATE \`User\` SET \`background\` = '${background}' WHERE \`User_ID\` = ${id}`);
-          msg.channel.send('Successfully updated your profile.');
-        }
-      });
-    });
-  };
 
-  // This is the thing that makes you set the emblems and the stuff. Trust me you're gonna want this too
-  client.setEmblem = (id, avatar, emblem, amount, msg) => {
-    client.checkUser(id, avatar, () => {
-      connection.query(`SELECT COUNT(*) AS hasB FROM \`userE\` WHERE \`userID\` = ${id} AND \`emblem\` = '${emblem}'`, (err1, res1, fields1) => {
-        if (res1[0].hasB === 0) {
-          connection.query(`SELECT *  FROM \`User\` WHERE \`User_ID\` = ${id}`, (err, res, fields) => {
-            if (err)
-              throw err;
-            if (res[0].Credits < amount) {
-              msg.channel.send("I'm sorry you don't have enough money for this emblem.");
-            } else {
-              connection.query(`UPDATE \`User\` SET \`emblem\` = '${emblem}', \`Credits\`=\`Credits\` -${amount} WHERE \`User_ID\` = ${id}`);
-              connection.query(`INSERT INTO \`userE\` (\`userID\`, \`emblem\`) VALUES (${id}, '${emblem}')`);
-              msg.channel.send('Successfully bought your emblem.');
-            }
-          });
-        } else {
-          connection.query(`UPDATE \`User\` SET \`emblem\` = '${emblem}' WHERE \`User_ID\` = ${id}`);
-          msg.channel.send('Successfully updated your profile.');
-        }
-      });
-    });
-  };
+	// Trust me you're gonna wanna add this.
 
-  // Emote stats updater
-  client.emoteCheck = (server_id, emote_id) => {
-    connection.query(`SELECT COUNT(*) AS inD FROM \`emote\` WHERE \`server_id\` = ${server_id} AND \`emote_id\` = ${emote_id}`, (err, res) => {
-      if (err)
-        throw err;
-      if (res[0].inD === 0) {
-        connection.query(`INSERT INTO \`emote\` (\`server_id\`, \`emote_id\`) VALUES (${server_id}, ${emote_id})`);
-        connection.query(`UPDATE \`emote\` SET \`used\` = \`used\` + 1 WHERE \`server_id\` = ${server_id} AND \`emote_id\` = ${emote_id}`);
-      } else {
-        connection.query(`UPDATE \`emote\` SET \`used\` = \`used\` + 1 WHERE \`server_id\` = ${server_id} AND \`emote_id\` = ${emote_id}`);
-      }
-    });
-  };
+	client.setBackground = (id, avatar, background, amount, msg) => {
+		try {
+			client.checkUser(id, avatar, () => {
+				connection.query(`SELECT COUNT(*) AS hasB FROM \`userB\` WHERE \`User_ID\` = ${id} AND \`name\` = '${background}'`, (err1, res1) => {
+					if (res1[0].hasB === 0) {
+						connection.query(`SELECT *  FROM \`User\` WHERE \`User_ID\` = ${id}`, (err, res) => {
+							if (err)
+								throw err;
+							if (res[0].Credits < amount) {
+								msg.channel.send('I\'m sorry you don\'t have enough money for this background.');
+							} else {
+								connection.query(`UPDATE \`User\` SET \`background\` = '${background}', \`Credits\`=\`Credits\` -${amount} WHERE \`User_ID\` = ${id}`);
+								connection.query(`INSERT INTO \`userB\` (\`User_ID\`, \`name\`) VALUES (${id}, '${background}')`);
+								msg.channel.send('Successfully bought your background.');
+							}
+						});
+					} else {
+						connection.query(`UPDATE \`User\` SET \`background\` = '${background}' WHERE \`User_ID\` = ${id}`);
+						msg.channel.send('Successfully updated your profile.');
+					}
+				});
+			});
+		} catch (e) {
+			client.error(msg.content, e);
+		}
+	};
 
-  // Search youtube
-  client.youtubeSearch = (searchType, type, maxRes, q) => {
-    const https = require('https');
-    return new Promise((resolve) => {
-      https.get(`https://www.googleapis.com/youtube/v3/${searchType}?part=snippet&maxRes=${maxRes}&q=${q}&type=${type}&key=${client.config.youtube.key}`, (res) => {
-        let body = '';
-        res.on('data', (chunk) => {
-          body += chunk;
-        });
-        res.on('end', () => {
-          resolve(body);
-        });
-      });
-    });
-  };
+	// This is the thing that makes you set the emblems and the stuff. Trust me you're gonna want this too
+	client.setEmblem = (id, avatar, emblem, amount, msg) => {
+		try {
+			client.checkUser(id, avatar, () => {
+				connection.query(`SELECT COUNT(*) AS hasB FROM \`userE\` WHERE \`userID\` = ${id} AND \`emblem\` = '${emblem}'`, (err1, res1) => {
+					if (res1[0].hasB === 0) {
+						connection.query(`SELECT *  FROM \`User\` WHERE \`User_ID\` = ${id}`, (err, res) => {
+							if (err)
+								throw err;
+							if (res[0].Credits < amount) {
+								msg.channel.send('I\'m sorry you don\'t have enough money for this emblem.');
+							} else {
+								connection.query(`UPDATE \`User\` SET \`emblem\` = '${emblem}', \`Credits\`=\`Credits\` -${amount} WHERE \`User_ID\` = ${id}`);
+								connection.query(`INSERT INTO \`userE\` (\`userID\`, \`emblem\`) VALUES (${id}, '${emblem}')`);
+								msg.channel.send('Successfully bought your emblem.');
+							}
+						});
+					} else {
+						connection.query(`UPDATE \`User\` SET \`emblem\` = '${emblem}' WHERE \`User_ID\` = ${id}`);
+						msg.channel.send('Successfully updated your profile.');
+					}
+				});
+			});
+		} catch (e) {
+			client.error(msg.content, e);
+		}
+	};
+
+	// Emote stats updater
+	client.emoteCheck = (server_id, emote_id) => {
+		try {
+			connection.query(`SELECT COUNT(*) AS inD FROM \`emote\` WHERE \`server_id\` = ${server_id} AND \`emote_id\` = ${emote_id}`, (err, res) => {
+				if (err)
+					throw err;
+				if (res[0].inD === 0) {
+					connection.query(`INSERT INTO \`emote\` (\`server_id\`, \`emote_id\`) VALUES (${server_id}, ${emote_id})`);
+					connection.query(`UPDATE \`emote\` SET \`used\` = \`used\` + 1 WHERE \`server_id\` = ${server_id} AND \`emote_id\` = ${emote_id}`);
+				} else {
+					connection.query(`UPDATE \`emote\` SET \`used\` = \`used\` + 1 WHERE \`server_id\` = ${server_id} AND \`emote_id\` = ${emote_id}`);
+				}
+			});
+		} catch (e) {
+			client.error(e);
+		}
+	};
+
+	// Search youtube
+	client.youtubeSearch = (searchType, type, maxRes, q) => {
+		const https = require('https');
+		return new Promise((resolve) => {
+			https.get(`https://www.googleapis.com/youtube/v3/${searchType}?part=snippet&maxRes=${maxRes}&q=${q}&type=${type}&key=${client.config.youtube.key}`, (res) => {
+				let body = '';
+				res.on('data', (chunk) => {
+					body += chunk;
+				});
+				res.on('end', () => {
+					resolve(body);
+				});
+			});
+		});
+	};
+  
+	client.fetchGuildMember = (msg) => {
+		let args = msg.content.slice(client.prefix.length).split(' ');
+
+		if (!args[1])
+			return false;
+		let m = msg.guild.members.get(args[1]) ||
+		msg.guild.members.find((m) => m.displayName.toLowerCase() === args[1].toLowerCase()) ||
+		msg.guild.members.find((m) => m.username.toLowerCase() === args[1].toLowerCase());
+		return m;
+
+	};
 };
